2734bf9af4e8495453f63522b44368a90c91de3d
ningweijian <ningweijian@artisansvalley.com>
2020-10-23 10:00:38 +0800

    refactor: 调整项目信息文件

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:cookiej/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    // Build our app and trigger a frame
     await tester.pumpWidget(CookieJ());
 
     // Verify that our counter starts at 0.
